64e468500720e722fa8d1744e7bd5cf2d198d4d5
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2021-11-19 11:04:04 +0300

    Basic types for DB

---
+++ db/types.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class User:
+    user_id: int
+    first_name: str
+    last_name: Optional[int]
+    tournament_id: Optional[int]
+    money: int = 1000  # TODO: probably should be float?
+
+
+@dataclass
+class Tournament:
+    tournament_id: int
+    start_time: datetime
+    end_time: datetime
+
+
+@dataclass
+class Stock:
+    ticker: str
+    fetch_date: datetime
+    price: int
