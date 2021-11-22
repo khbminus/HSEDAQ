c4ed03c231d230d264faa311589122f905ada5c2
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2021-11-22 23:26:37 +0300

    Okay, now I got stock!

--- db/stocks.py
+++ db/stocks.py
@@ -4,16 +4,31 @@ import psycopg
 from loguru import logger
 from psycopg.rows import class_row
 from pandas import DataFrame
+import datetime
 
 
 def update_stocks(stocks_frame: DataFrame) -> None:
     logger.debug(f"Updating {stocks_frame.shape[0]} stocks")
-    for ticker in stocks_frame.columns.levels[0]:
-        processed_list = [(ticker, timestamp.to_pydatetime(), price) for timestamp, price in
-                          zip(stocks_frame.index, stocks_frame[ticker]["Open"])]
-        with psycopg.connect(dbname=DB_NAME, autocommit=True) as conn:
-            with conn.cursor(row_factory=class_row(Stock)) as cur:
+
+    with psycopg.connect(dbname=DB_NAME, autocommit=True) as conn:
+        with conn.cursor(row_factory=class_row(Stock)) as cur:
+            for ticker in stocks_frame.columns.levels[0]:
+                current_stocks = stocks_frame[ticker]["Open"].dropna()
+                processed_list = [(ticker, timestamp.to_pydatetime(), price) for timestamp, price in
+                                  zip(current_stocks.index, current_stocks)]
                 cur.executemany("""INSERT INTO stocks(ticker, fetch_date, price)
-        VALUES (%s, %s, %s)
-        ON CONFLICT DO NOTHING; """, processed_list)
+                VALUES (%s, %s, %s)
+                ON CONFLICT DO NOTHING; """, processed_list)
     logger.debug(f"Saved {stocks_frame.shape[0]} stocks")
+
+
+def get_stock(symbol: str) -> Stock:
+    date_now = datetime.datetime.now()
+    logger.debug(f"Getting stock {symbol} at {date_now}")
+    with psycopg.connect(dbname=DB_NAME, autocommit=True) as conn:
+        with conn.cursor(row_factory=class_row(Stock)) as cur:
+            res = cur.execute("SELECT * from stocks "
+                              "where fetch_date = (SELECT max(fetch_date) "
+                              "from stocks where ticker = %s and fetch_date < %s) "
+                              "and ticker=%s;", (symbol, date_now, symbol)).fetchone()
+    return res
