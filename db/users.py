2cca12da2bd9a48d0196f778469f9c0b0251a1a6
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2021-11-22 23:41:42 +0300

    Refactored users.py

--- db/users.py
+++ db/users.py
@@ -20,10 +20,11 @@ def save_user(user: User) -> None:
     with psycopg.connect(dbname=DB_NAME, autocommit=True) as conn:
         with conn.cursor(row_factory=class_row(User)) as cur:
             cur.execute("""INSERT INTO users(user_id, first_name, last_name, tournament_id, money) 
-VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (user_id) DO UPDATE 
-  SET first_name = excluded.first_name, 
-      last_name = excluded.last_name,
-      tournament_id = excluded.tournament_id,
-      money = excluded.money;""", (user.user_id, user.first_name, user.last_name, user.tournament_id, user.money))
+                            VALUES (%s, %s, %s, %s, %s)
+                            ON CONFLICT (user_id) DO UPDATE 
+                              SET first_name = excluded.first_name, 
+                                  last_name = excluded.last_name,
+                                  tournament_id = excluded.tournament_id,
+                                  money = excluded.money;""",
+                        (user.user_id, user.first_name, user.last_name, user.tournament_id, user.money))
     logger.debug(f"Saved {user.user_id}")
