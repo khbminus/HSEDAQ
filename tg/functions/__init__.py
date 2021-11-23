1a028f3c6c6ab5543510f4629f45fa949bea96b6
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2021-11-23 12:14:31 +0300

    Selected parse_mode

--- tg/functions/__init__.py
+++ tg/functions/__init__.py
@@ -1,3 +1,3 @@
 from tg.functions.getUserInfo import command_get_user
-
-__all__ = ['command_get_user']
+from tg.functions.createTournament import command_create_tournament
+__all__ = ['command_get_user', 'command_create_tournament']
