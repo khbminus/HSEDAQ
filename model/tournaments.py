eede6e2376d1ef6c7b838e07269c44261a2bf3cc
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2021-11-23 12:15:11 +0300

    Changed create_tournanment arguments(duration -> end_time) and fixed next_id error

--- model/tournaments.py
+++ model/tournaments.py
@@ -2,15 +2,18 @@ import time
 
 from db.types import Tournament  # TODO: move types from db?
 from db.tournaments import get_all_tournaments, save_tournament, get_tournament_participants
-from datetime import datetime, timedelta
+from datetime import datetime
 from view.tournaments import send_finish_statistics
 
 
 # TODO: options?
-def create_tournament(start_time: datetime, duration: timedelta) -> Tournament:
+def create_tournament(start_time: datetime, end_time: datetime) -> Tournament:
     tournaments = get_all_tournaments()
-    next_id = max(tournaments, key=lambda x: x.tournament_id).tournament_id
-    tour = Tournament(tournament_id=next_id, start_time=start_time, end_time=start_time + duration)
+    if len(tournaments) == 0:
+        next_id = 0
+    else:
+        next_id = max(tournaments, key=lambda x: x.tournament_id).tournament_id + 1  # TODO: query db?
+    tour = Tournament(tournament_id=next_id, start_time=start_time, end_time=end_time)
     save_tournament(tour)
     return tour
 
@@ -26,4 +29,3 @@ def tournaments_polling() -> None:
     while True:
         complete_pending_tournaments()
         time.sleep(30)  # TODO: Add as parameter?
-
