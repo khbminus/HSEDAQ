db0717dee6e1b09ba8380432066a961fb21a5c84
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2021-11-23 10:17:55 +0300

    Some refactor in model

--- model/tournaments.py
+++ model/tournaments.py
@@ -1,8 +1,9 @@
 import time
 
 from db.types import Tournament  # TODO: move types from db?
-from db.tournaments import get_all_tournaments, save_tournament
+from db.tournaments import get_all_tournaments, save_tournament, get_tournament_participants
 from datetime import datetime, timedelta
+from view.tournaments import send_finish_statistics
 
 
 # TODO: options?
@@ -14,16 +15,14 @@ def create_tournament(start_time: datetime, duration: timedelta) -> Tournament:
     return tour
 
 
-def completed_pending_tournaments() -> None:
+def complete_pending_tournaments() -> None:
     time_now = datetime.now()  # TODO: replace with db query?
     pending = list(filter(lambda t: t.end_time <= time_now and not t.is_ended, get_all_tournaments()))
     for tournament in pending:
-        # TODO: Add closing in View
-        pass
+        send_finish_statistics(tournament, get_tournament_participants(tournament))
 
 
 def tournaments_polling() -> None:
     while True:
+        complete_pending_tournaments()
         time.sleep(30)  # TODO: Add as parameter?
-
-
