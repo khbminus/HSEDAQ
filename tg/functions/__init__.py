64902e3ce981db1cdfcc857d465cda43bacc1505
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2021-11-26 16:13:00 +0300

    Status command

--- tg/functions/__init__.py
+++ tg/functions/__init__.py
@@ -1,4 +1,6 @@
 from tg.functions.getUserInfo import command_get_user
 from tg.functions.createTournament import command_create_tournament
 from tg.functions.enterTournament import command_enter_tournament
-__all__ = ['command_get_user', 'command_create_tournament', 'command_enter_tournament']
+from tg.functions.statusCommand import command_status
+
+__all__ = ['command_get_user', 'command_create_tournament', 'command_enter_tournament', 'command_status']
