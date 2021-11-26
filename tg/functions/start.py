bd1efba6f4787e5257f9ea9f7202ab2f548a0c7c
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2021-11-26 16:17:41 +0300

    Merged

---
+++ tg/functions/start.py
@@ -0,0 +1,24 @@
+from tg.bot import Bot
+from telebot.types import Message
+from db.types import User
+from db.users import get_user, save_user
+from loguru import logger
+
+bot = Bot().bot
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message: Message):
+    cid = message.chat.id
+    uid = message.from_user.id
+
+    user = get_user(uid)
+    if user is None:
+        logger.error(f"User {user} isn't have record in the database.")
+        user = User(
+            user_id=uid,
+            first_name=message.from_user.first_name,
+            last_name=message.from_user.last_name)
+        save_user(user)
+
+    bot.send_message(chat_id=cid, text=f"Welcome, {user.first_name}. Use `/help` for list of commands.")
