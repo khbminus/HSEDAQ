2de4bf610c19c4702e166c4e22f9ad5a56330853
Sergey Kharitontsev-Beglov <serega.haritontsev@gmail.com>
2021-11-26 12:05:37 +0300

    Fixed typo

--- tg/functions/__init__.py
+++ tg/functions/__init__.py
@@ -1,3 +1,4 @@
 from tg.functions.getUserInfo import command_get_user
 from tg.functions.createTournament import command_create_tournament
-__all__ = ['command_get_user', 'command_create_tournament']
+from tg.functions.enterTournament import command_enter_tournament
+__all__ = ['command_get_user', 'command_create_tournament', 'command_enter_tournament']
